3b7e4a69cea64acd8bb436593ce2bab60730c7c3
sulav182 <sulavpoudyal@hotmail.com>
2023-02-15 22:14:05 -0600

    removed unnecessary libraries

--- section 1/plaintext_alice/plaintext_alice.ino
+++ section 1/plaintext_alice/plaintext_alice.ino
@@ -1,36 +1,4 @@
 
-// Crypto - Version: Latest 
-#include <AES.h>
-#include <AuthenticatedCipher.h>
-#include <BLAKE2b.h>
-#include <BLAKE2s.h>
-#include <BigNumberUtil.h>
-#include <BlockCipher.h>
-#include <CTR.h>
-#include <ChaCha.h>
-#include <ChaChaPoly.h>
-#include <Cipher.h>
-#include <Crypto.h>
-#include <Curve25519.h>
-#include <EAX.h>
-#include <Ed25519.h>
-#include <GCM.h>
-#include <GF128.h>
-#include <GHASH.h>
-#include <Hash.h>
-#include <KeccakCore.h>
-#include <NoiseSource.h>
-#include <OMAC.h>
-#include <P521.h>
-#include <Poly1305.h>
-#include <RNG.h>
-#include <SHA256.h>
-#include <SHA3.h>
-#include <SHA512.h>
-#include <SHAKE.h>
-#include <XOF.h>
-#include <XTS.h>
-
 // demo: CAN-BUS Shield, send data
 // loovee@seeed.cc
 
